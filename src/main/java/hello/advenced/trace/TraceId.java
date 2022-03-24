7b781d4262c3dd6ec94760b5b4039a334c470f3d
JinMinChoi <jinmin2216@naver.com>
2022-03-24 18:47:59 +0900

    feat, test: Add log trace service

---
+++ src/main/java/hello/advenced/trace/TraceId.java
@@ -0,0 +1,38 @@
+package hello.advenced.trace;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class TraceId {
+
+    private String id; // 트랜잭션 ID (DB 트랜잭션 X)
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8); //생성된 uuid의 앞 8자리 (중복은 될 수 있지만 크게 문제는 되지 않는다.)
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+}
---
+++ src/main/java/hello/advenced/trace/TraceStatus.java
@@ -0,0 +1,14 @@
+package hello.advenced.trace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TraceStatus {
+
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+}
