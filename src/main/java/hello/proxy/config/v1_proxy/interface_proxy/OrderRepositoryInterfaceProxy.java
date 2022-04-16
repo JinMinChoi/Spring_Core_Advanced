bb371ba068526cf5769a718a0e885913bd129627
JinMinChoi <jinmin2216@naver.com>
2022-04-16 16:33:03 +0900

    feat: Apply JDK dynamic proxy First.

--- src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
+++ src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
@@ -1,18 +1,15 @@
-package hello.proxy.app.v2_proxy.concrete_proxy;
+package hello.proxy.config.v1_proxy.interface_proxy;
 
-import hello.proxy.app.v2.OrderRepositoryV2;
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.app.v1.OrderRepositoryV1;
+import lombok.RequiredArgsConstructor;
 
-public class OrderRepositoryConcreteProxy extends OrderRepositoryV2 {
+@RequiredArgsConstructor
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
 
-    private final OrderRepositoryV2 target;
+    private final OrderRepositoryV1 target;
     private final LogTrace logTrace;
-
-    public OrderRepositoryConcreteProxy(OrderRepositoryV2 target, LogTrace logTrace) {
-        this.target = target;
-        this.logTrace = logTrace;
-    }
 
     @Override
     public void save(String itemId) {
